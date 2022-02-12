cbb736a9c0fcc776db8816172df61e980ed946fd
Derek Howe <derekhowe.com@gmail.com>
2022-02-12 16:22:56 +0000

    Set up console

---
+++ src/Command/ConvertTempCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+// src/Command/HashPasswordCommand.php
+
+namespace App\Command;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class ConvertTempCommand extends Command
+{
+    /**
+     * In this method setup command, description, and its parameters
+     */
+    protected function configure()
+    {
+       
+    }
+
+    /**
+     * Here all logic happens
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+       
+    }
+}
+
+?>
